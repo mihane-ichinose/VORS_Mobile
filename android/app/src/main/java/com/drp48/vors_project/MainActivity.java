9a997aff2f04039bb10c0824c4e0d7c96de8c872
sj4918 <sj4918@ic.ac.uk>
2021-06-08 00:40:59 +0800

    Initial skeleton with a login page example

---
+++ android/app/src/main/java/com/drp48/vors_project/MainActivity.java
@@ -0,0 +1,6 @@
+package com.drp48.vors_project;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/users.dart
@@ -0,0 +1,6 @@
+const mockUsers = {
+  'sj4918@ic.ac.uk': '12345',
+  'hunter@gmail.com': 'hunter',
+  'near.huscarl@gmail.com': 'subscribe to pewdiepie',
+  '@.com': '.',
+};
