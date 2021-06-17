4561c91bf100ba33e29db84aff309bb679b1a3f4
Shitian Jin <shitian.jin18@imperial.ac.uk>
2021-06-17 16:03:16 +0800

    Put back main page home to loginPage.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:vors_project/login_page.dart';
 import 'package:vors_project/restaurant_page.dart';
 import 'package:vors_project/signup_page.dart';
 import 'package:vors_project/user_page.dart';
-import 'package:vors_project/order_page.dart';
 
 const apiUrl = "http://84.238.224.41:5005/";
 int customerId = 0;
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Futura.bold",
       ),
-      home: new OrderPage(7),
+      home: new LoginPage(),
       routes: {
         "/logout": (context) => new LoginPage(),
         "/login": (context) => new RestaurantPage(0, ""),
